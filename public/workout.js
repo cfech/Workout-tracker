f16d78ba076daf56f45d0002a96e8208b27308e1
Connor <cfech44@gmail.com>
2020-03-26 22:44:28 -0400

    adding duration variable

--- public/workout.js
+++ public/workout.js
@@ -5,11 +5,12 @@ async function initWorkout() {
     document
       .querySelector("a[href='/exercise?']")
       .setAttribute("href", `/exercise?id=${lastWorkout._id}`);
-
+      console.log(lastWorkout.exercises[0].duration)
     const workoutSummary = {
       totalDuration: lastWorkout.exercises[0].duration,
       date: formatDate(lastWorkout.day),
-      totalDuration: lastWorkout.totalDuration,
+      // totalDuration: lastWorkout.totalDuration,
+     
       numExercises: lastWorkout.exercises.length,
       ...tallyExercises(lastWorkout.exercises)
     };
